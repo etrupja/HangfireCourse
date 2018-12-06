29236ff0ede3c42f29d8ea729454edf0591f4e3b
ERVIS-PC\Ervis <ervistrupja@outlook.com>
2018-12-06 21:44:59 +0100

    UserController Added + DbContext

---
+++ HangfireCourse/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HangfireCourse.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HangfireCourse.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ HangfireCourse/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangfireCourse.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
